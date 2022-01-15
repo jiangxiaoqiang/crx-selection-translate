8aa528b862c9b9c64922f4b066ff8ff36aed73da
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-01-15 18:25:16 +0800

    [feature] tweak

--- src/background-scripts/reddwarf/auth/reddwarf-auth.js
+++ src/background-scripts/reddwarf/auth/reddwarf-auth.js
@@ -1,10 +1,15 @@
+
+import { Auth,Validate } from "js-wheel";
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendReponse) {
     debugger;
     switch (request.action) {
+        
         case 'reddwarf_authorize':
             alert("reddwarf auth");
             let authorize_url = `https://api.poenhub.top/oauth2/authorize/?response_type=token`
-            window.open(authorize_url)
+            //Auth.handleAccessTokenExpire();
+            Validate.mobileCheck('2222');
             sendReponse()
             break;
         default:
